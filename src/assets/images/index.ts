8c67fdaba1094c1e11d4ccd5020f551a4c63d4b4
Lucas Suota <lucassuotass@gmail.com>
2024-01-08 16:36:50 -0300

    docs: adding images, svgs

---
+++ src/assets/images/index.ts
@@ -0,0 +1,5 @@
+import FullLogoBlack from "./fulllogo_white.png";
+import LogoWhite from "./logo_white.png";
+import LogoBlack from "./logo_black.png";
+
+export { FullLogoBlack, LogoBlack, LogoWhite };
