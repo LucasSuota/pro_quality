1842e76bae5b2618a105f954a73b0add27375a7a
Lucas Suota <lucassuotass@gmail.com>
2024-01-12 16:23:34 -0300

    feat: framer motion added on the whyproquality section

--- components/WhyProQuality.tsx
+++ components/WhyProQuality.tsx
@@ -1,9 +1,27 @@
+"use client";
+
 import { weGuaranteeList } from "@/constants";
 import { WeGuaranteCard } from ".";
+import { motion } from "framer-motion";
 
 const WhyProQuality = () => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { staggerChildren: 0.3 } },
+  };
+
+  const itemVariants = {
+    hidden: { y: 20, opacity: 0 },
+    visible: { y: 0, opacity: 1 },
+  };
+
   return (
-    <section className="max-w-screen-lg mx-auto text-center mt-20 py-20 bg-progray">
+    <motion.section
+      className="max-w-screen-lg mx-auto text-center mt-20 py-20 bg-progray"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <h2 className="w-[200px] mx-auto leading-7 font-proquality text-proTitles font-bold">
         POR QUE A PROQUALITY?
       </h2>
@@ -25,10 +43,12 @@ const WhyProQuality = () => {
       </p>
       <div className="sm:mx-20 mx-10 mt-10 flex flex-col sm:flex-row">
         {weGuaranteeList.map((item, index) => (
-          <WeGuaranteCard key={index} title={item.title} logo={item.logo} />
+          <motion.div key={index} variants={itemVariants}>
+            <WeGuaranteCard title={item.title} logo={item.logo} />
+          </motion.div>
         ))}
       </div>
-    </section>
+    </motion.section>
   );
 };
 
