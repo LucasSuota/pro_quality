39b420d17c8c672d8c6df90938e036a672f62baf
Lucas Suota <lucassuotass@gmail.com>
2024-01-21 17:46:16 -0300

    feat: footer updated

--- components/Buttons/QueroOrcamento copy.tsx
+++ components/Buttons/QueroOrcamento copy.tsx
@@ -4,7 +4,7 @@ const VerMais = () => {
   return (
     <Link
       href={"/contato"}
-      className="sm:w-2/5 w-full bg-problue active:bg-progray active:text-problack mt-4 sm:w-1/4 w-3/4 py-2 text-center font-proquality text-lg font-light text-white"
+      className="sm:w-2/5 w-full bg-problue hover:bg-problack active:bg-progray active:text-problack mt-4 sm:w-1/4 w-3/4 py-2 text-center font-proquality text-lg font-light text-white transition-all"
     >
       Ver mais
     </Link>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,15 +4,15 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-full bg-problack mx-auto mt-20">
-      <div className="max-w-screen-lg px-4 py-4">
+    <footer className="w-full bg-problack mx-auto mt-20 flex justify-center">
+      <div className="max-w-screen-lg px-4 py-4 flex flex-col items-center justify-center gap-2">
         <Image
           src={LogoWhite}
           width={162}
           height={162}
           alt="Pro Quality Logo"
         />
-        <h4 className="text-white text-sm">
+        <h4 className="text-white sm:text-sm text-xs">
           2024. Todos os direitos reservados
         </h4>
       </div>
