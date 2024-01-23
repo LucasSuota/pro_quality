bccb4764047c13b347cc09cd3fcde2fbc4513672
Lucas Suota <lucassuotass@gmail.com>
2024-01-23 13:11:33 -0300

    style: quem somos now has highlighted words

--- app/quem_somos/page.tsx
+++ app/quem_somos/page.tsx
@@ -16,7 +16,6 @@ export default function Home() {
         <ActionButton />
         <Header />
         <Presentation />
-
         <Footer />
       </Transition>
     </>
--- components/Presentation.tsx
+++ components/Presentation.tsx
@@ -1,32 +1,54 @@
-import React from "react";
+"use client";
+
+import { useAnimate } from "framer-motion";
+import { useEffect } from "react";
 
 const Presentation = () => {
+  const [scope, animate] = useAnimate();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      animate(
+        "span",
+        { backgroundColor: "#195973", color: "white" },
+        { duration: 1 }
+      );
+
+      clearInterval(interval);
+    }, 500);
+
+    return () => {
+      animate("span", { opacity: 1 });
+    };
+  });
+
   return (
-    <section className="w-full mx-auto h-screen">
+    <section className="w-full mx-auto">
       <div className="max-w-screen-lg mx-auto px-10 sm:px-0 pt-40 flex sm:flex-row flex-col gap-10">
         <p className="text-7xl font-proquality font-bold">&quot;</p>
-        <div className="sm:w-full">
+        <div ref={scope} className="sm:w-full">
           <h2 className="sm:text-titles text-smtitles font-proquality font-bold">
             Quem somos
           </h2>
           <p className="sm:text-para text-smpara mt-4">
-            A Proquality Engenharia é uma empresa dedicada à excelência nos
-            serviços de ensaios em obras de pavimentação e construção civil, bem
-            como vistorias de entrega de obras.
+            A <span>ProQuality Engenharia</span> é uma empresa dedicada à
+            excelência nos serviços de ensaios em obras de pavimentação e
+            construção civil, bem como vistorias de entrega de obras.
           </p>
           <p className="sm:text-para text-smpara mt-4">
-            Com uma equipe altamente capacitada e comprometida, nosso objetivo é
-            assegurar a qualidade e conformidade em cada projeto que assumimos.
+            Com uma equipe <span>altamente capacitada</span> e{" "}
+            <span>comprometida</span>, nosso objetivo é assegurar a{" "}
+            <span>qualidade</span> e conformidade em cada projeto que assumimos.
             Realizamos ensaios especializados para garantir a durabilidade e
             segurança das estruturas, além de oferecer vistorias abrangentes que
             antecipam potenciais problemas, proporcionando tranquilidade aos
             nossos clientes.
           </p>
           <p className="sm:text-para text-smpara mt-4">
-            Na Proquality Engenharia, comprometimento, conhecimento técnico e
-            eficiência são os pilares que fundamentam nossa atuação,
-            tornando-nos a escolha confiável para suas necessidades de controle
-            de qualidade em obras.
+            Na Proquality Engenharia, <span>comprometimento</span>,{" "}
+            <span>conhecimento técnico</span> e<span>eficiência</span> são os
+            pilares que fundamentam nossa atuação, tornando-nos a escolha
+            confiável para suas necessidades de controle de qualidade em obras.
           </p>
         </div>
         <p className="text-7xl font-proquality font-bold">&quot;</p>
