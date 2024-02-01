407e7a331823e5566c5cca562e1757e5944c988a
Lucas Suota <lucassuotass@gmail.com>
2024-02-01 16:24:51 -0300

    style: footer style

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,75 +3,52 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import Icon from "@mui/material/Icon";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
 
 const Footer = () => {
+  const footerItems = [
+    { title: "Home", link: "/" },
+    { title: "Serviços", link: "/servicos" },
+    { title: "Quem somos", link: "/quem_somos" },
+    { title: "Contato", link: "/contato" },
+  ];
+
   return (
-    // <footer className="bg-problack text-white py-8">
-    //   <div className="container mx-auto flex flex-row justify-between">
-    //     <div className="w-full md:w-1/3 mb-4 md:mb-0">
-    //       <Image
-    //         src={FullLogoWhite}
-    //         width={98}
-    //         height={98}
-    //         alt="Pro Quality Logo"
-    //       />
-    //     </div>
-
-    //     {/* Column 2 - Pages */}
-    //     <div className="w-full md:w-1/3 mb-4 md:mb-0 font-proquality">
-    //       <Link href={"/"}>
-    //         <p className="hover:text-problue">Home</p>
-    //       </Link>
-    //       <Link href={"/quem_somos"}>
-    //         <p className="hover:text-problue">Quem somos</p>
-    //       </Link>
-    //       <Link href={"/servicos"}>
-    //         <p className="hover:text-problue">Serviços</p>
-    //       </Link>
-    //       <Link href={"/contato"}>
-    //         <p className="hover:text-problue">Contato</p>
-    //       </Link>
-    //     </div>
-
     //     {/* Column 3 - Social Icons */}
     //     <div className="w-full md:w-1/3 flex justify-center items-center">
     //       <Link href={"https://www.linkedin.com/in/maiconorlowski/"}>
-    //         Linkedin
     //       </Link>
-    //       <a
-    //         href="https://api.whatsapp.com/"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //         className="text-white"
-    //       >
-    //         <i className="fab fa-whatsapp"></i>
-    //       </a>
+    //       <Link href={"https://www.linkedin.com/in/maiconorlowski/"}>
+    //         <WhatsAppIcon className="w-[64px] h-[64px]" />
+    //       </Link>
     //     </div>
     //   </div>
     // </footer>
-    <footer className="w-full bg-problack mx-auto flex justify-center mt-40">
-      <div className="max-w-screen-lg px-4 py-4 flex sm:flex-row flex-col sm:items-center items-start justify-center sm:gap-20 gap-4">
-        <div className="flex flex-col gap-2">
+    <footer className="w-full bg-problack">
+      <div className="max-w-screen-lg px-4 py-6 mx-auto flex flex-row items-center justify-center sm:gap-40 gap-4">
+        <div>
           <Image
             src={FullLogoWhite}
-            width={162}
-            height={162}
-            alt="Pro Quality Logo"
+            width={100}
+            height={100}
+            alt="ProQuality logo"
           />
         </div>
-        <div className="font-proquality flex flex-col gap-2 text-white">
-          <Link href={"/"}>
-            <p className="hover:text-problue">HOME</p>
+        <nav className="flex sm:flex-row flex-col gap-4">
+          {footerItems.map((item, index) => (
+            <Link href={item.link} key={index}>
+              <p className="font-proquality sm:text-para text-smpara text-white hover:text-progray">
+                {item.title}
+              </p>
+            </Link>
+          ))}
+        </nav>
+        <div className="flex flex-col sm:flex-row gap-4">
+          <Link href={"https://www.linkedin.com/in/maiconorlowski/"}>
+            <LinkedInIcon className="w-[32px] h-[32px] text-white hover:text-progray" />
           </Link>
-          <Link href={"/quem_somos"}>
-            <p className="hover:text-problue">QUEM SOMOS</p>
-          </Link>
-          <Link href={"/servicos"}>
-            <p className="hover:text-problue">SERVIÇOS</p>
-          </Link>
-          <Link href={"/contato"}>
-            <p className="hover:text-problue">CONTATO</p>
+          <Link href={"https://www.linkedin.com/in/maiconorlowski/"}>
+            <WhatsAppIcon className="w-[32px] h-[32px] text-white hover:text-progray" />
           </Link>
         </div>
       </div>
