190c9960ee409030ccba9b80dfffa1648f7d50c3
Lucas Suota <lucassuotass@gmail.com>
2024-02-05 12:18:21 -0300

    style: quem somos now have a better style

--- components/Presentation.tsx
+++ components/Presentation.tsx
@@ -1,54 +1,31 @@
+import Image from "next/image";
+import { CivilEngineer, FullLogoBlack } from "@/public/assets/images";
+import WhatsApp from "@mui/icons-material/WhatsApp";
 import DefaultButton from "./Buttons/DefualtButton";
+import Link from "next/link";
 
 const Presentation = () => {
   return (
-    // <section className="w-full mx-auto">
-    //   <div className="max-w-screen-lg mx-auto px-4 pt-40 pb-40 flex flex-col">
-    //     <div ref={scope} className="sm:w-full">
-    //       <h2 className="sm:text-titles text-smtitles font-proquality font-bold">
-    //         Quem somos
-    //       </h2>
-    //       <p className="sm:text-para text-smpara mt-4">
-    //         A <span>PROQUALITY Engenharia</span> é uma empresa dedicada à
-    //         excelência nos serviços de ensaios em obras de{" "}
-    //         <span>pavimentação</span> e <span>construção civil</span>, bem como{" "}
-    //         <span>vistorias</span> de entrega de obras.
-    //       </p>
-    //       <p className="sm:text-para text-smpara mt-4">
-    //         Com uma equipe <span>altamente capacitada</span> e comprometida,
-    //         nosso objetivo é assegurar a <span>qualidade</span> e{" "}
-    //         <span>conformidade</span> em cada projeto que assumimos. Realizamos
-    //         ensaios especializados para garantir a<span>durabilidade</span> e{" "}
-    //         <span>segurança</span> das estruturas, além de oferecer vistorias
-    //         abrangentes que antecipam potenciais problemas, proporcionando
-    //         tranquilidade aos nossos clientes.
-    //       </p>
-    //       <p className="sm:text-para text-smpara mt-4">
-    //         Na <span>PROQUALITY Engenharia</span>, <span>comprometimento</span>,{" "}
-    //         <span>conhecimento técnico</span> e <span>eficiência</span> são os
-    //         pilares que fundamentam nossa atuação, tornando-nos a escolha
-    //         confiável para suas necessidades de controle de qualidade em obras.
-    //       </p>
-    //     </div>
-    //     <QueroOrcamento />
-    //   </div>
-    // </section>
-    <section className="max-w-screen-lg mx-auto sm:px-10 px-4 min-h-screen flex flex-col">
-      <div className="mt-40">
-        <h2 className="text-problack text-titles font-proquality font-bold">
-          Quem somos
-        </h2>
-        <div className="font-proquality sm:text-para text-smpara">
-          <p className="mt-4">
-            A{" "}
-            <span className="underline underline-offset-2 decoration-problue">
-              ProQuality Engenharia
-            </span>{" "}
-            é uma empresa dedicada à excelência nos serviços de ensaios em obras
-            de pavimentação e construção civil, bem como vistorias de entrega de
-            obras.
+    <section>
+      <div className="max-w-screen-lg mx-auto flex gap-10 flex-row items-center justify-between">
+        <div className="relative w-2/4 h-screen">
+          <Image
+            src={CivilEngineer}
+            style={{ objectFit: "cover" }}
+            fill
+            alt="Logo"
+          />
+        </div>
+        <div className="w-2/4 h-screen flex flex-col gap-4 items-start justify-center font-proquality">
+          <h3 className="text-5xl mb-4 text-problue font-bold ">
+            PROQUALITY ENGENHARIA
+          </h3>
+          <p>
+            A ProQuality Engenharia é uma empresa dedicada à excelência nos
+            serviços de ensaios em obras de pavimentação e construção civil, bem
+            como vistorias de entrega de obras.
           </p>
-          <p className="mt-4">
+          <p>
             Com uma equipe altamente capacitada e comprometida, nosso objetivo é
             assegurar a qualidade e conformidade em cada projeto que assumimos.
             Realizamos ensaios especializados para garantir a durabilidade e
@@ -56,15 +33,16 @@ const Presentation = () => {
             antecipam potenciais problemas, proporcionando tranquilidade aos
             nossos clientes.
           </p>
-          <p className="mt-4">
+          <p>
             Na Proquality Engenharia, comprometimento, conhecimento técnico e
             eficiência são os pilares que fundamentam nossa atuação,
             tornando-nos a escolha confiável para suas necessidades de controle
             de qualidade em obras.
           </p>
+
+          <Image src={FullLogoBlack} width={130} height={130} alt="logo" />
         </div>
       </div>
-      <DefaultButton title="Ver Serviços e Projetos" link="/servicos" />
     </section>
   );
 };
--- public/assets/images/index.ts
+++ public/assets/images/index.ts
@@ -3,5 +3,13 @@ import LogoWhite from "./logo_white.png";
 import LogoBlack from "./logo_black.png";
 import HeroBg from "./herobg.png";
 import FullLogoBlack from "./fulllogo_black.png";
+import CivilEngineer from "./civilengineer.jpg";
 
-export { FullLogoWhite, LogoBlack, LogoWhite, HeroBg, FullLogoBlack };
+export {
+  FullLogoWhite,
+  LogoBlack,
+  LogoWhite,
+  HeroBg,
+  FullLogoBlack,
+  CivilEngineer,
+};
