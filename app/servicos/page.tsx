8f0a7b5e218520979f025e5c14d23b3ece056e04
Lucas Suota <lucassuotass@gmail.com>
2024-02-06 10:52:32 -0300

    style: transition when page loads added to services

--- app/servicos/page.tsx
+++ app/servicos/page.tsx
@@ -1,17 +1,20 @@
+"use client";
+
 import { Footer, Header } from "@/components";
-import DefaultButton from "@/components/Buttons/DefualtButton";
 import ServicosHero from "@/components/Servicos/ServicosHero";
 import ServicosPrestados from "@/components/Servicos/ServicosPrestados";
+import Transition from "@/components/Transition/Transition";
 import { services } from "@/constants";
 
 const page = () => {
   return (
     <section>
-      <Header />
-      <ServicosHero />
-      <ServicosPrestados services={services} />
-
-      <Footer />
+      <Transition>
+        <Header />
+        <ServicosHero />
+        <ServicosPrestados services={services} />
+        <Footer />
+      </Transition>
     </section>
   );
 };
